3152c2a6bd82fec41b959cc21c032123fcb75224
Leonardo Santos <leofrrodrigues86@gmail.com>
2024-09-25 14:21:31 -0300

    retirado o auth das rotas da task

--- back-end/src/routes/Tasks.router.ts
+++ back-end/src/routes/Tasks.router.ts
@@ -1,36 +1,36 @@
 import { Router } from "express";
 import { TasksController } from "../controllers/Tasks.Controller";
-import { auth } from "../middleware/auth";
+//import { auth } from "../middleware/auth";
 
 export const tasksRouter = Router();
 const tasksController = new TasksController();
 
 tasksRouter.post(
-  "/api/auth/task/create-task",
-  auth,
-  tasksController.createTask
+    "/api/auth/task/create-task",
+
+    tasksController.createTask
 );
 
 tasksRouter.get(
-  "/api/auth/home/get-all-tasks",
-  auth,
-  tasksController.getAllTasks
+    "/api/auth/home/get-all-tasks",
+
+    tasksController.getAllTasks
 );
 
 tasksRouter.post(
-  "/api/auth/task/edit-task",
-  auth,
-  tasksController.editTaskById
+    "/api/auth/task/edit-task",
+
+    tasksController.editTaskById
 );
 
 tasksRouter.get(
-  "/api/auth/task/get-task/:id",
-  auth,
-  tasksController.getTaskById
+    "/api/auth/task/get-task/:id",
+
+    tasksController.getTaskById
 );
 
 tasksRouter.delete(
-  "/api/auth/task/delete-task/:id",
-  auth,
-  tasksController.deleteTaskById
+    "/api/auth/task/delete-task/:id",
+
+    tasksController.deleteTaskById
 );
